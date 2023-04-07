3e44ddbcf2fcff8261b7578c074d9ec10ac5e343
alanliew <alanliew@coingecko.com>
2023-04-07 15:00:57 +0800

    newtableformed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
   #get 'home/index'
   get 'home/about'
   root 'home#index'
+  get 'friends/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
